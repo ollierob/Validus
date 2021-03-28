46b3c3a4bc8c6aa67b82900c7bea6d04d5ac496f
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2021-03-28 17:21:11 +0100

    Core interfaces

--- analyzer/core/src/main/java/net/ollie/validus/analysis/SpecificationAnalysis.java
+++ analyzer/core/src/main/java/net/ollie/validus/analysis/SpecificationAnalysis.java
@@ -2,11 +2,15 @@ package net.ollie.validus.analysis;
 
 import net.ollie.validus.analysis.satisfaction.Satisfaction;
 import net.ollie.validus.analysis.verification.Verification;
+import net.ollie.validus.specification.Specification;
 
 import javax.annotation.Nonnull;
 import java.util.Set;
 
 public interface SpecificationAnalysis {
+
+    @Nonnull
+    Specification specification();
 
     default boolean isSatisfied() {
         return !this.satisfactions().isEmpty();
---
+++ analyzer/core/src/main/java/net/ollie/validus/specification/Specification.java
@@ -0,0 +1,5 @@
+package net.ollie.validus.specification;
+
+public record Specification(SpecificationId id) {
+
+}

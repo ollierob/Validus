a27a9be98ac3e41b4272861e15045c84bb0d1a49
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2021-04-02 20:21:07 +0100

    Website

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -1,3 +1,26 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {}
+module.exports = {
+    output: {
+        path: __dirname + "/target/classes/js",
+        filename: "[name].[contenthash].bundle.js",
+        chunkFilename: "[name].[contenthash].bundle.js",
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json", ".less"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: ["ts-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    mode: 'development',
+    devtool: 'inline-source-map'
+}

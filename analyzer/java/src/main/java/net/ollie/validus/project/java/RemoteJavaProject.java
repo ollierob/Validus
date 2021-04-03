837a1c22d5e1c30196ec7769db22fb4fc59a25df
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2021-04-03 22:02:20 +0100

    Projects

--- analyzer/java/src/main/java/net/ollie/validus/project/java/RemoteJavaProject.java
+++ analyzer/java/src/main/java/net/ollie/validus/project/java/RemoteJavaProject.java
@@ -2,15 +2,6 @@ package net.ollie.validus.project.java;
 
 import net.ollie.validus.project.FromRemoteProject;
 
-import javax.annotation.Nonnull;
-import java.util.concurrent.CompletableFuture;
-
 public interface RemoteJavaProject extends JavaProject, FromRemoteProject<LocalJavaProject> {
-
-    @Nonnull
-    @Override
-    default CompletableFuture<LocalJavaProject> toLocalProject() {
-        return FromRemoteProject.super.toLocalProject();
-    }
 
 }

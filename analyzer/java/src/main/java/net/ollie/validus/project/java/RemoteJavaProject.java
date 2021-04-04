10bade387c05b9dd9ab03893d2d789113b2f1bd5
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2021-04-04 20:59:38 +0100

    Projects

--- analyzer/java/src/main/java/net/ollie/validus/project/java/RemoteJavaProject.java
+++ analyzer/java/src/main/java/net/ollie/validus/project/java/RemoteJavaProject.java
@@ -1,7 +1,30 @@
 package net.ollie.validus.project.java;
 
 import net.ollie.validus.project.FromRemoteProject;
+import net.ollie.validus.project.LocalProject;
+import net.ollie.validus.project.RemoteProject;
 
-public non-sealed interface RemoteJavaProject extends JavaProject, FromRemoteProject<LocalJavaProject> {
+import javax.annotation.Nonnull;
+import java.net.URL;
+import java.nio.file.Path;
+
+public record RemoteJavaProject(RemoteProject remote, Path javaPath) implements JavaProject, FromRemoteProject<LocalJavaProject> {
+
+    public RemoteJavaProject(final RemoteProject remote) {
+        this(remote, null);
+    }
+
+    @Nonnull
+    @Override
+    public URL resolveUrl(final JavaMethod method) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Nonnull
+    @Override
+    public LocalJavaProject transform(final LocalProject project) {
+        final var javaPath = this.javaPath == null ? project.root() : project.root().resolve(this.javaPath);
+        return new LocalJavaProject(javaPath, this);
+    }
 
 }
--- projects/core/src/main/java/net/ollie/validus/project/LocalProject.java
+++ projects/core/src/main/java/net/ollie/validus/project/LocalProject.java
@@ -1,11 +1,15 @@
 package net.ollie.validus.project;
 
 import javax.annotation.Nonnull;
+import java.nio.file.Path;
 
 public interface LocalProject extends Project {
 
     @Nonnull
     RemoteProject remote();
+
+    @Nonnull
+    Path root();
 
     @Nonnull
     @Override

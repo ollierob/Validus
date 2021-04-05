767236c34bc4de405d58589905561460c6608f96
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2021-04-05 16:33:55 +0100

    Website

--- website/src/main/js/main/Main.tsx
+++ website/src/main/js/main/Main.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {LeftMenu, RightContent, TopMenu} from "./Layout";
+import "./Layout.css"
 
 class Main extends React.PureComponent {
 

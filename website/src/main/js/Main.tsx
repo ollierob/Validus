1a2e6cad081afbb5f12ac4d2d3f6a267914390ef
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2021-04-05 14:31:51 +0100

    Website build

---
+++ website/src/main/js/Main.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+class Main extends React.PureComponent {
+
+}
+
+ReactDOM.render(<Main/>, document.getElementById("main"));

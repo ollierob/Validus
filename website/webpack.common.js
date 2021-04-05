0a7df8496bc83250d11d3c80b8bb84c15d57150b
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2021-04-05 14:23:23 +0100

    Website build

---
+++ website/webpack.common.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: ["./src/main/js/Main.tsx"]
+    },
+    output: {
+        path: __dirname + "/target/classes/js",
+        filename: "[name].[contenthash].bundle.js",
+        chunkFilename: "[name].[contenthash].bundle.js",
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json", ".less"]
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all",
+            maxInitialRequests: Infinity,
+            minSize: 0,
+            maxSize: 1000000,
+            cacheGroups: {
+                vendor: {
+                    name: "vendors",
+                    test: /node_modules/
+                }
+            }
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: ["ts-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: false,
+            chunks: ["main"],
+            publicPath: "/js",
+            template: "src/main/resources/page.html.template",
+            filename: "main.html"
+        })
+        ]
+}
